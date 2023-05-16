5dd6475cda8b1d1fd448d039e32c9396bc95718c
Tim Wright <wrightt@microsoft.com>
2023-05-17 10:38:52 +1200

    fix comment

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ type Fn<ARGS extends any[], R> = (...args: ARGS) => R;
 
 const useEventCallback = <A extends any[], R>(fn: Fn<A, R>): Fn<A, R> => {
   const ref = useRef<Fn<A, R>>(fn);
+  
+  // not sure this I like assigning to a ref during the method call, but React recommends it for certain circumstances:
+  // https://react.dev/reference/react/useRef#avoiding-recreating-the-ref-contents
+  // so it's probably OK.
   if (ref.current !== fn) {
     ref.current = fn;
   }
-
-  // not sure this I like assigning to a ref during the method call, but it's what other libraries do.
-  // however, React recommends it for certain circumstances: https://react.dev/reference/react/useRef#avoiding-recreating-the-ref-contents
-  // so it's probably OK.
 
   const immutableRef = useRef((...args: A) => {
     // perform call on version of the callback from last commited render
